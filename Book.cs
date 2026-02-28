0e963cc87349e498a21b06fc7340ae071c571235
Hafsat Dere <derehafsat24@gmail.com>
2026-02-28 11:33:19 +0100

    correction

--- Book.cs
+++ Book.cs
@@ -5,7 +5,6 @@ class Book
     public string Title { get; set; }
     public string Author { get; set; }
     public int Year { get; set; }
-    public string bookStatus = "Available";
     
     public Book(string title, string authorName, int year)
     {
